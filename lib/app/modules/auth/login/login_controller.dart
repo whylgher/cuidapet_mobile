2551b8395630ac5b6b6321bc44a718b4a4df9600
Whyll <whylgher@gmail.com>
2022-12-19 12:24:45 +0000

    Login e-mail e password (Firebase)

--- lib/app/modules/auth/login/login_controller.dart
+++ lib/app/modules/auth/login/login_controller.dart
@@ -1,6 +1,9 @@
+import 'package:cuidapet/app/core/exception/failure.dart';
 import 'package:cuidapet/app/core/ui/widgets/loader.dart';
+import 'package:cuidapet/app/core/ui/widgets/messages.dart';
 import 'package:mobx/mobx.dart';
 
+import '../../../core/exception/user_not_exists_exception.dart';
 import '../../../core/logger/app_logger.dart';
 import '../../../services/user/user_service.dart';
 
@@ -19,10 +22,20 @@ abstract class LoginControllerBase with Store {
         _log = log;
 
   Future<void> login(String login, String password) async {
-    Loader.show();
-    print(login);
-    print(password);
-    await Future.delayed(const Duration(seconds: 2));
-    Loader.hide();
+    try {
+      Loader.show();
+      await _userService.login(login, password);
+      Loader.hide();
+    } on Failure catch (e, s) {
+      final errorMessage = e.message ?? 'Erro ao realizar login';
+      _log.error(errorMessage, e, s);
+      Loader.hide();
+      Messages.alert(errorMessage);
+    } on UserNotExistsException {
+      const errorMessage = 'Usuário não cadastrado.';
+      _log.error(errorMessage);
+      Loader.hide();
+      Messages.alert(errorMessage);
+    }
   }
 }
--- lib/app/services/user/user_service_impl.dart
+++ lib/app/services/user/user_service_impl.dart
@@ -4,6 +4,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import './user_service.dart';
 import '../../core/exception/failure.dart';
 import '../../core/exception/user_exists_exception.dart';
+import '../../core/exception/user_not_exists_exception.dart';
 import '../../core/logger/app_logger.dart';
 
 class UserServiceImpl implements UserService {
@@ -37,6 +38,41 @@ class UserServiceImpl implements UserService {
     } on FirebaseException catch (e, s) {
       _log.error('Erro ao criar usuário no Firebase', e, s);
       throw Failure(message: 'Erro ao criar usuário.');
+    }
+  }
+
+  @override
+  Future<void> login(String email, String password) async {
+    try {
+      final firebaseAuth = FirebaseAuth.instance;
+      final logindMethods =
+          await firebaseAuth.fetchSignInMethodsForEmail(email);
+
+      if (logindMethods.isEmpty) {
+        throw UserNotExistsException();
+      }
+
+      if (logindMethods.contains('password')) {
+        final userCredential = await firebaseAuth.signInWithEmailAndPassword(
+            email: email, password: password);
+
+        final userVerfied = userCredential.user?.emailVerified ?? false;
+
+        if (!userVerfied) {
+          userCredential.user?.sendEmailVerification();
+
+          throw Failure(
+              message:
+                  'e-mail não confirmado, por favor conferir a caixa de spam');
+        }
+      } else {
+        throw Failure(
+            message:
+                'Login não pode ser feito por email e password, por favor, utilize outro método, facebook ou google.');
+      }
+    } on FirebaseAuthException catch (e, s) {
+      _log.error('Usuário ou senha inválidos FirebaseAuthError: [{$e.code}]');
+      throw Failure(message: 'Usuária ou senha inválidos!!!');
     }
   }
 }
